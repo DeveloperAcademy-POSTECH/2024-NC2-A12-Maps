e696274d321e97d5032ff20ffc750c96d6a0bc80
TopiaYu <raemo963@naver.com>
2024-06-14 21:12:31 +0900

    Initial Commit

---
+++ MC2Maps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC2Maps
+//
+//  Created by donghwan on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC2Maps/MC2MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2MapsApp.swift
+//  MC2Maps
+//
+//  Created by donghwan on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MC2MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
