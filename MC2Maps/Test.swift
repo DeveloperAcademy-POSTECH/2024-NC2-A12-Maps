3fc16a48d6290dbecbc27278db667d5dc48e0335
TopiaYu <raemo963@naver.com>
2024-06-19 14:02:10 +0900

    test

---
+++ MC2Maps/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  MC2Maps
+//
+//  Created by donghwan on 6/19/24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(" World!")
+    }
+}
+
+#Preview {
+    Test()
+}
